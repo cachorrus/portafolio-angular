46df6acc60070a33af7cc98947220882f0fea286
DESKTOP-FIPOO7U\cachorro <23436971+cachorrus@users.noreply.github.com>
2018-08-09 12:04:23 -0500

    add template Masonry add HeaderComponent add FooterComponent add PortafolioComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortafolioComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
